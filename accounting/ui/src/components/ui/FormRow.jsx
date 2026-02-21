1454607a029ce613dbc8a48c76113aface6b341d
BrunZo <ziger.bruno@gmail.com>
2026-02-20 23:54:09 -0300

    purge(accounting): clean up

--- accounting/ui/src/components/ui/FormRow.jsx
+++
@@ -1,8 +0,0 @@
-export function FormRow({ label, children, className = '' }) {
-  return (
-    <div className={`form-row ${className}`}>
-      {label && <label>{label}</label>}
-      {children}
-    </div>
-  );
-}
